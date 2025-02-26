1368c81ea429e863ecdd7d39bd993c4f096cdeb1
meir <contact@flamingo.dev>
2025-02-26 09:56:04 +0100

    update plugin script

--- lua/human/patch/sunglasses.lua
+++
@@ -1,7 +0,0 @@
--- This is a patch for the sunglasses plugin. This patch is needed because the
--- plugin gives an error when the "Sunglasses" tag is not present on a window
--- which happens often when switching between buffers using Harpoon2
-require("sunglasses.window").is_shaded = function(self)
-  local response, _ = pcall(vim.api.nvim_win_get_var, self.window, "Sunglasses")
-  return response == true
-end
