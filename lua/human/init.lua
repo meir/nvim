fd050f030b2b92452ac0718056139d7b2be92d37
meir <contact@flamingo.dev>
2025-09-11 14:07:23 +0200

    rename file

--- lua/human/init.lua
+++ lua/human/init.lua
@@ -1,7 +1,7 @@
 require("human.commands")
 require("human.options")
 require("human.keymaps")
-require("human.lazy")
+require("human.plugins")
 
 vim.cmd.autocmd("ColorScheme", "*", "let g:colors_name = expand('<amatch>')")
 vim.cmd("ColorschemeReset")
