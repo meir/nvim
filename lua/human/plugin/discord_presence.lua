8ee7b1c7ce4ffee5b0047820f88a77f87874a987
meir <contact@flamingo.dev>
2025-09-16 13:26:23 +0200

    fix discord presence

--- lua/human/plugin/discord_presence.lua
+++ lua/human/plugin/discord_presence.lua
@@ -5,10 +5,15 @@ return {
     editor = {
       client = "neovim",
       tooltip = "E-macs",
-      icon = "https://github.com/meir/nvim/blob/main/NeovimShadowed.png?raw=true",
+      icon = "https://github.com/meir/nvim/blob/main/Neovim.png?raw=true",
     },
     display = {
       swap_icons = true,
-    }
+    },
+    text = {
+        editing = 'Editing ${filename}',
+        file_browser = 'Browsing files in ${tooltip}',
+    },
+    variables = true,
   },
 }
